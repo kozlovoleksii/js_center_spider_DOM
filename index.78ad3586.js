b7bdef528c22c560498de64da2ced94f97b5e25c
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-06-27 16:49:13 +0300

    make build

---
+++ index.78ad3586.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=t.clientHeight,i=e.clientHeight,c=e.clientWidth,n=t.clientHeight;e.style.top=n/2-i/2+"px",e.style.left=l/2-c/2+"px";
+//# sourceMappingURL=index.78ad3586.js.map
